3042d62ceb179a4c39a22251ced761691f9ad888
ReenaShiny <52193771+ReenaShiny@users.noreply.github.com>
2019-07-22 22:38:15 +0530

    Update alpa.c

--- alpa.c
+++ alpa.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main()
 {
-char ch;
-scanf("%c",&ch);
-if((ch>=97)&&(ch<=122)||((ch>=65)&&(ch<=90))
+char c;
+scanf("%c",&c);
+if((c>=97)&&(c<=122)||(c>=65)&&(c<=90))
 {printf("Alphabet");}
 else
 {printf("No");}
