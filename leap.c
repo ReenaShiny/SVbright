1c4fc56225726e307bef7c72e9cb26b919cbd2dd
ReenaShiny <52193771+ReenaShiny@users.noreply.github.com>
2019-07-23 15:47:36 +0530

    Update leap.c

--- leap.c
+++ leap.c
@@ -3,8 +3,11 @@ int main()
 {
 int a;
 scanf("%d",&a);
-if(a%4==0 || a%100!=0)
+if(a%4==0 || a%400==0)
 {printf("yes");}
+else if(a%100!=0)
+{printf("no");}
 else
-{printf("no");}}
+  {printf("no");}
+}
 
